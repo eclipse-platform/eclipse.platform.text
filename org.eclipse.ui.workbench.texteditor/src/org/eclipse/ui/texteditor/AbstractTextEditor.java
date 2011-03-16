8cda758641dad3aba5f1d51c9fa965d9e7f809db
Dani Megert <dmegert>
2011-03-16 16:26:32 +0000

    Backported fix for bug 337719: [implementation] AbstractTextEditor does not prompt when out of sync in MultiPageEditorPart

--- org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/AbstractTextEditor.java
+++ org.eclipse.ui.workbench.texteditor/src/org/eclipse/ui/texteditor/AbstractTextEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -976,7 +976,7 @@ public abstract class AbstractTextEditor extends EditorPart implements ITextEdit
 			if (fIsHandlingActivation)
 				return;
 
-			if (fActivePart == AbstractTextEditor.this) {
+			if (fActivePart == AbstractTextEditor.this || fActivePart != null && fActivePart.getAdapter(AbstractTextEditor.class) == AbstractTextEditor.this) {
 				fIsHandlingActivation= true;
 				try {
 					safelySanityCheckState(getEditorInput());
