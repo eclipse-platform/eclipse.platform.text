eclipse-platform/eclipse.platform.ui

commit 78fc9f6f6566323c691f1fee1eb14de2f27e7d34

Improve async completion popup for completion on typing.
The popup is shown on type event of a letter or digit if the async
completion is active.
The async completion popup always shows the progress and the resulting
message of completions. The fix improves it so if async completion is
triggered automatically, then the popup is only shown if there are
completions to be shown.
A general improvement is done to close the popup when last filtered
offset is preceded by a space.

org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/AsyncCompletionProposalPopup.java
@@ -21,6 +21,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -36,6 +37,7 @@ import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 
 import org.eclipse.core.runtime.SafeRunner;
 
@@ -65,6 +67,8 @@ class AsyncCompletionProposalPopup extends CompletionProposalPopup {
 	private CompletableFuture<?> fAggregatedPopulateFuture;
 
 	private Collection<CompletableFuture<?>> toCancelFutures= new LinkedList<>();
+
+	private PopupVisibleTimer fPopupVisibleTimer= new PopupVisibleTimer();
 
 	private static final class ComputingProposal implements ICompletionProposal, ICompletionProposalExtension {
 
@@ -203,7 +207,8 @@ class AsyncCompletionProposalPopup extends CompletionProposalPopup {
 				return;
 			}
 
-			if (autoInsert && count == 1 && !autoActivated && canAutoInsert(fComputedProposals.get(0))) {
+			if (autoInsert && count == 1 && !autoActivated &&
+					fContentAssistant.isAutoActivateCompletionOnType() && canAutoInsert(fComputedProposals.get(0))) {
 				insertProposal(fComputedProposals.get(0), (char) 0, 0, offset);
 				hide();
 			} else {
@@ -254,8 +259,16 @@ class AsyncCompletionProposalPopup extends CompletionProposalPopup {
 						if (!fComputedProposals.contains(computingProposal) && callback != null) {
 							callback.accept(fComputedProposals);
 						} else {
-							setProposals(fComputedProposals, false);
-							displayProposals();
+							boolean stillComputing= fComputedProposals.contains(computingProposal);
+							boolean hasProposals= (stillComputing && fComputedProposals.size() > 1)
+									|| (!stillComputing && !fComputedProposals.isEmpty());
+
+							if ((autoActivated && hasProposals) || !autoActivated) {
+								setProposals(fComputedProposals, false);
+								displayProposals(true);
+							} else if (isValid(fProposalShell) && !fProposalShell.isVisible() && remaining.get() == 0) {
+								hide(); // we only tear down if the popup is not visible.
+							}
 						}
 					});
 				}
@@ -264,7 +277,19 @@ class AsyncCompletionProposalPopup extends CompletionProposalPopup {
 			fAggregatedPopulateFuture= CompletableFuture.allOf(populateFutures.toArray(new CompletableFuture[populateFutures.size()]));
 			toCancelFutures.add(fAggregatedPopulateFuture);
 		}
-		displayProposals();
+		displayProposals(!autoActivated);
+	}
+
+	@Override
+	void displayProposals(boolean showPopup) {
+		if (showPopup) {
+			fPopupVisibleTimer.stop();
+		}
+
+		super.displayProposals(showPopup);
+		if (!showPopup) {
+			fPopupVisibleTimer.start();
+		}
 	}
 
 	@Override
@@ -330,6 +355,7 @@ class AsyncCompletionProposalPopup extends CompletionProposalPopup {
 
 	@Override
 	public void hide() {
+		fPopupVisibleTimer.stop();
 		super.hide();
 		cancelFutures();
 	}
@@ -380,4 +406,40 @@ class AsyncCompletionProposalPopup extends CompletionProposalPopup {
 		return IDocument.DEFAULT_CONTENT_TYPE;
 	}
 
+	private class PopupVisibleTimer implements Runnable {
+		private Thread fThread;
+
+		private Object fMutex= new Object();
+
+		private int fAutoActivationDelay= 500;
+
+		protected void start() {
+			fThread= new Thread(this, JFaceTextMessages.getString("ContentAssistant.assist_delay_timer_name")); //$NON-NLS-1$
+			fThread.start();
+		}
+
+		@Override
+		public void run() {
+			try {
+				while (true) {
+					synchronized (fMutex) {
+						if (fAutoActivationDelay != 0)
+							fMutex.wait(fAutoActivationDelay);
+					}
+					Optional<Display> display= Optional.ofNullable(fContentAssistSubjectControlAdapter.getControl()).map(Control::getDisplay);
+					display.ifPresent(d -> d.asyncExec(() -> displayProposals(true)));
+					break;
+				}
+			} catch (InterruptedException e) {
+			}
+			fThread= null;
+		}
+
+		protected void stop() {
+			Thread threadToStop= fThread;
+			if (threadToStop != null && threadToStop.isAlive())
+				threadToStop.interrupt();
+		}
+
+	}
 }

org.eclipse.jface.text/META-INF/MANIFEST.MF
@@ -2,7 +2,7 @@ Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Bundle-Name: %pluginName
 Bundle-SymbolicName: org.eclipse.jface.text
-Bundle-Version: 3.20.200.qualifier
+Bundle-Version: 3.21.0.qualifier
 Bundle-Vendor: %providerName
 Bundle-Localization: plugin
 Export-Package: 

org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/CompletionProposalPopup.java
@@ -1219,10 +1219,21 @@ class CompletionProposalPopup implements IContentAssistListener {
 	}
 
 	/**
-	 * Displays this popup and install the additional info controller, so that additional info
-	 * is displayed when a proposal is selected and additional info is available.
+	 * Displays this popup and install the additional info controller, so that additional info is
+	 * displayed when a proposal is selected and additional info is available.
 	 */
 	void displayProposals() {
+		displayProposals(true);
+	}
+
+	/**
+	 * Displays this popup and install the additional info controller, so that additional info is
+	 * displayed when a proposal is selected and additional info is available.
+	 *
+	 * @param showPopup if true show the popup or otherwise initialize the popup and listeners
+	 *            without showing the popup.
+	 */
+	void displayProposals(boolean showPopup) {
 
 		if (!isValid(fProposalShell) || !isValid(fProposalTable))
 			return;
@@ -1231,45 +1242,55 @@ class CompletionProposalPopup implements IContentAssistListener {
 
 			ensureDocumentListenerInstalled();
 
-			if (fFocusHelper == null) {
-				fFocusHelper= new IEditingSupport() {
-
-					@Override
-					public boolean isOriginator(DocumentEvent event, IRegion focus) {
-						return false; // this helper just covers the focus change to the proposal shell, no remote editions
-					}
-
-					@Override
-					public boolean ownsFocusShell() {
-						return true;
-					}
-
-				};
+			if (showPopup) {
+				configureAndMakeVisible();
 			}
-			if (fViewer instanceof IEditingSupportRegistry) {
-				IEditingSupportRegistry registry= (IEditingSupportRegistry) fViewer;
-				registry.register(fFocusHelper);
-			}
-
-
-			/* https://bugs.eclipse.org/bugs/show_bug.cgi?id=52646
-			 * on GTK, setVisible and such may run the event loop
-			 * (see also https://bugs.eclipse.org/bugs/show_bug.cgi?id=47511)
-			 * Since the user may have already canceled the popup or selected
-			 * an entry (ESC or RETURN), we have to double check whether
-			 * the table is still okToUse. See comments below
-			 */
-			fProposalShell.setVisible(true); // may run event loop on GTK
-			// transfer focus since no verify key listener can be attached
-			if (!fContentAssistSubjectControlAdapter.supportsVerifyKeyListener() && isValid(fProposalShell))
-				fProposalShell.setFocus(); // may run event loop on GTK ??
-
-			if (fAdditionalInfoController != null && isValid(fProposalTable)) {
-				fAdditionalInfoController.install(fProposalTable);
-				fAdditionalInfoController.handleTableSelectionChanged();
-			}
+		} else if (!fProposalShell.isVisible() && showPopup) {
+			// if a previous call did initialize but did make the popup visible, this will make sure the popup will be visible.
+			configureAndMakeVisible();
 		} else
 			hide();
+	}
+
+	private void configureAndMakeVisible() {
+		if (fFocusHelper == null) {
+			fFocusHelper= new IEditingSupport() {
+
+				@Override
+				public boolean isOriginator(DocumentEvent event, IRegion focus) {
+					return false; // this helper just covers the focus change to the proposal shell, no remote editions
+				}
+
+				@Override
+				public boolean ownsFocusShell() {
+					return true;
+				}
+
+			};
+		}
+
+		if (fViewer instanceof IEditingSupportRegistry) {
+			IEditingSupportRegistry registry= (IEditingSupportRegistry) fViewer;
+			registry.register(fFocusHelper);
+		}
+
+
+		/* https://bugs.eclipse.org/bugs/show_bug.cgi?id=52646
+		 * on GTK, setVisible and such may run the event loop
+		 * (see also https://bugs.eclipse.org/bugs/show_bug.cgi?id=47511)
+		 * Since the user may have already canceled the popup or selected
+		 * an entry (ESC or RETURN), we have to double check whether
+		 * the table is still okToUse. See comments below
+		 */
+		fProposalShell.setVisible(true); // may run event loop on GTK
+		// transfer focus since no verify key listener can be attached
+		if (!fContentAssistSubjectControlAdapter.supportsVerifyKeyListener() && isValid(fProposalShell))
+			fProposalShell.setFocus(); // may run event loop on GTK ??
+
+		if (fAdditionalInfoController != null && isValid(fProposalTable)) {
+			fAdditionalInfoController.install(fProposalTable);
+			fAdditionalInfoController.handleTableSelectionChanged();
+		}
 	}
 
 	/**
@@ -1381,6 +1402,16 @@ class CompletionProposalPopup implements IContentAssistListener {
 				e.doit= false;
 				fProposalShell.setFocus();
 				return false;
+
+			case SWT.BS: {
+				try {
+					if (fFilterOffset > 0 && fContentAssistSubjectControlAdapter.getDocument().getChar(fFilterOffset - 1) == SWT.SPACE)
+						hide();
+				} catch (BadLocationException e1) {
+					// ignore error
+				}
+				break;
+			}
 
 			default:
 				if (fContentAssistant.isCompletionProposalTriggerCharsEnabled()) {

org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/ContentAssistant.java
@@ -1079,6 +1079,15 @@ public class ContentAssistant implements IContentAssistant, IContentAssistantExt
 	private boolean fCompletionProposalTriggerCharsEnabled= true;
 
 	/**
+	 * Tells whether this completion list is shown on each valid character which is either a letter
+	 * or digit. This works conjunction with {@link #fAsynchronous}
+	 *
+	 * @since 3.20
+	 */
+	private boolean fAutoActivateCompletionOnType= false;
+
+
+	/**
 	 * Creates a new content assistant. The content assistant is not automatically activated,
 	 * overlays the completion proposals with context information list if necessary, and shows the
 	 * context information above the location at which it was activated. If auto activation will be
@@ -1103,6 +1112,7 @@ public class ContentAssistant implements IContentAssistant, IContentAssistantExt
 	public ContentAssistant(boolean asynchronous) {
 		fPartitioning= IDocumentExtension3.DEFAULT_PARTITIONING;
 		fAsynchronous= asynchronous;
+		enableAutoActivateCompletionOnType(Boolean.getBoolean("org.eclipse.jface.assist.activateCompletionOnType")); //$NON-NLS-1$
 	}
 
 	/**
@@ -1228,6 +1238,11 @@ public class ContentAssistant implements IContentAssistant, IContentAssistantExt
 		if (processors == null) {
 			return TriggerType.NONE;
 		}
+
+		if (fAutoActivateCompletionOnType && (Character.isLetter(c) || Character.isDigit(c))) {
+			return TriggerType.COMPLETION_PROPOSAL;
+		}
+
 		for (IContentAssistProcessor processor : processors) {
 			IContentAssistProcessorExtension extension= IContentAssistProcessorExtension.adapt(processor);
 			if (extension.isCompletionProposalAutoActivation(c, fViewer, offset)) {
@@ -2747,4 +2762,20 @@ public class ContentAssistant implements IContentAssistant, IContentAssistantExt
 		return fIsAutoActivated;
 	}
 
+	/**
+	 * Sets whether this completion list is shown on each valid character which is either a letter
+	 * or digit. This works conjunction with {@link #fAsynchronous}
+	 *
+	 * @param enable whether or not to enable this feature
+	 * @since 3.21
+	 */
+	public final void enableAutoActivateCompletionOnType(boolean enable) {
+		if (fAsynchronous) {
+			fAutoActivateCompletionOnType= enable;
+		}
+	}
+
+	boolean isAutoActivateCompletionOnType() {
+		return fAutoActivateCompletionOnType;
+	}
 }

eclipse-platform/eclipse.platform.text

commit c6cdfa3bc20f8a2d38cf4d044af25eb9e4b73bd0

Hide the popup in async mode when there are no completions left
Fix bug a introduced in 78fc9f6f6566323c691f1fee1eb14de2f27e7d34 that
prevented the popup from closing when there are were completions left.

In addition, create the variable stillComputing earlier so that it can
be reused.

org.eclipse.jface.text/src/org/eclipse/jface/text/contentassist/AsyncCompletionProposalPopup.java
@@ -244,9 +244,10 @@ class AsyncCompletionProposalPopup extends CompletionProposalPopup {
 						if (offset != fInvocationOffset || fComputedProposals != requestSpecificProposals) {
 							return;
 						}
+						boolean stillComputing= fComputedProposals.contains(computingProposal);
 						if (autoInsert
 								&& !autoActivated
-								&& !fComputedProposals.contains(computingProposal)
+								&& !stillComputing
 								&& fComputedProposals.size() == 1
 								&& remaining.get() == 0
 								&& canAutoInsert(fComputedProposals.get(0))) {
@@ -256,18 +257,17 @@ class AsyncCompletionProposalPopup extends CompletionProposalPopup {
 							}
 							return;
 						}
-						if (!fComputedProposals.contains(computingProposal) && callback != null) {
+						if (!stillComputing && callback != null) {
 							callback.accept(fComputedProposals);
 						} else {
-							boolean stillComputing= fComputedProposals.contains(computingProposal);
 							boolean hasProposals= (stillComputing && fComputedProposals.size() > 1)
 									|| (!stillComputing && !fComputedProposals.isEmpty());
 
 							if ((autoActivated && hasProposals) || !autoActivated) {
 								setProposals(fComputedProposals, false);
 								displayProposals(true);
-							} else if (isValid(fProposalShell) && !fProposalShell.isVisible() && remaining.get() == 0) {
-								hide(); // we only tear down if the popup is not visible.
+							} else if (isValid(fProposalShell) && fProposalShell.isVisible() && remaining.get() == 0) {
+								hide(); // we only tear down if the popup is visible.
 							}
 						}
 					});